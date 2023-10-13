47147be069b9a15b3343d4654333032b74d21cd1
Thomas Mattern <113630601+ThomasMattern2@users.noreply.github.com>
2023-10-13 15:27:48 -0600

    Update deliv4.py

--- deliv4.py
+++ deliv4.py
@@ -1,73 +1,102 @@
+# Import necessary libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pandas import Series, DataFrame
+
+# Read Titanic data from a CSV file into a Pandas DataFrame
 titanic_data = pd.read_csv(r'titanic_excel.csv')
 df = pd.DataFrame(titanic_data)
-dfx = df.drop(columns=['Name','Sex','Survived'])
-sy=df.Survived
 
+# Drop specific columns from the DataFrame
+dfx = df.drop(columns=['Name', 'Sex', 'Survived'])
+sy = df.Survived
+
+# Import train_test_split function from scikit-learn to split the dataset into training and testing sets
 from sklearn.model_selection import train_test_split
-dfx_train, dfx_test, sy_train, sy_test= train_test_split(dfx,sy)
-dfx_train, dfx_test, sy_train, sy_test;
+dfx_train, dfx_test, sy_train, sy_test = train_test_split(dfx, sy)
 
+# Import preprocessing functions for label encoding and feature scaling
 from sklearn import preprocessing
+
+# Label encode the 'Survived' column for training data
 le = preprocessing.LabelEncoder()
 le = le.fit(sy_train)
-le.classes_;
+
+# Store the encoded classes
+le.classes_
+
+# Transform the target labels
 y_train = le.transform(sy_train)
-sy_train, y_train;
+
+# Feature scaling using Min-Max scaling
 n1 = preprocessing.MinMaxScaler()
 n1 = n1.fit(dfx_train.to_numpy())
+
+# Transform the training data
 X_train = n1.transform(dfx_train.to_numpy())
 
+# Import the KNeighborsClassifier for k-nearest neighbors classification
 from sklearn.neighbors import KNeighborsClassifier
+
+# Initialize variables for k values, test and training scores
 n = 81
 score_test = []
 score_train = []
 k_vals = []
-#looping through knn values to create the axis for the graphs
-for i in range(1,n):
+
+# Loop through different k values to build KNN models and evaluate them
+for i in range(1, n):
     knn = KNeighborsClassifier(n_neighbors=i)
-    knn = knn.fit(X_train,y_train)
+    knn = knn.fit(X_train, y_train)
+
+    # Transform the test data and labels
     X_test = n1.transform(dfx_test.to_numpy())
     y_test = le.transform(sy_test.to_numpy())
+
     k_vals.append(i)
-    score_test.append(knn.score(X_test,y_test))
-    score_train.append(knn.score(X_train,y_train))
-#graph plotting
+    score_test.append(knn.score(X_test, y_test))
+    score_train.append(knn.score(X_train, y_train))
+
+# Create a plot for the test scores with different k values
 xplot = np.array(k_vals)
 yplot = np.array(score_test)
-plt.title("scores for K values 1-80, Test dataset")
-plt.ylabel('score')
+plt.title("Scores for K values 1-80, Test dataset")
+plt.ylabel('Score')
 plt.xlabel('K')
 plt.plot(xplot, yplot)
 plt.show()
 
+# Create a plot for the training scores with different k values
 xplot = np.array(k_vals)
 yplot2 = np.array(score_train)
-plt.title("scores for K values 1-80, Train dataset")
-plt.ylabel('score')
+plt.title("Scores for K values 1-80, Train dataset")
+plt.ylabel('Score')
 plt.xlabel('K')
 plt.plot(xplot, yplot2)
 plt.show()
-#confusion matrix implimentation
+
+# Import confusion matrix related functions
 from sklearn.metrics import confusion_matrix, plot_confusion_matrix, ConfusionMatrixDisplay
 from sklearn.datasets import make_classification
 from sklearn.svm import SVC
+
+# Initialize lists to store true and predicted values
 new = []
 y_true = []
-y_pred= knn.predict(X_test)
+
+# Predict on the test data using the best k (from the previous loop)
+y_pred = knn.predict(X_test)
 i = 0
 while i < 222:
     b = y_pred[i]
     a = y_test[i]
     y_true.append(a)
     new.append(b)
-    i +=1
-matrix = confusion_matrix(y_true,new)
+    i += 1
+
+# Calculate the confusion matrix
+matrix = confusion_matrix(y_true, new)
 tn, fp, fn, tp = matrix.ravel()
-
-
 
 
